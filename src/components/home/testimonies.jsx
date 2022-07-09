dbe1ee79bb177789370372119f587a765bcc6ae9
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-07-09 18:52:00 +0530

    new design

--- src/components/home/testimonies.jsx
+++ src/components/home/testimonies.jsx
@@ -93,8 +93,8 @@ export default function Testimonies() {
       <div className="flex w-full bg-[#FDFDFD] bottom-10 h-[28vw] items-center justify-center">
         <div className="flex w-[100%] justify-center">
           <div
-            onClick={() => window.scrollTo(0, 400)}
-            className="w-48 h-48 z-50 flex flex-col items-center justify-center"
+            onClick={() => window.scrollTo(0, window.scrollY + 400)}
+            className="w-48 h-48 flex flex-col items-center justify-center"
           >
             <div className="button w-36 h-36 bg-transparent border-black hover:border-transparent transition-colors duration-300 ease-in-out hover:bg-[#F0C808] flex rounded-full border justify-center items-center">
               <Image src={ArrowDown} />
