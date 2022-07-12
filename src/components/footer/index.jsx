c10ec84e0764cffb1ccd1634e984dcb382569023
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-07-12 17:17:43 +0530

    mobile view

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 import Logo from "@/assets/INCUBR (1).svg";
 import DarkLogo from "@/assets/dark.svg";
-import Description from "../common/Description";
+import Link from "next/link";
 import WhatsApp from "@/assets/whitewhatsapp.svg";
 import Instagram from "@/assets/whiteinsta.svg";
 import LinkedIn from "@/assets/whitelinkedin.svg";
@@ -39,7 +39,9 @@ export default function Footer({ isDark = false }) {
           className={` h-screen px-10  space-y-10 lg:space-y-[2vw] w-full sm:w-[80%] lg:w-[80%] flex flex-col lg:flex-row items-center justify-center`}
         >
           <div className="flex flex-col space-y-10 lg:space-y-[3vw] justify-start items-start">
-            <Image src={isDark ? DarkLogo : Logo} />
+            <Link href={"/"}>
+              <Image src={isDark ? DarkLogo : Logo} />
+            </Link>
             <div
               className={`w-full text-md ${
                 isDark ? "text-[#FDFDFD]" : "text-[#1F1D1D]"
@@ -75,15 +77,17 @@ export default function Footer({ isDark = false }) {
             </div>
           </div>
           <div className="flex flex-col space-y-10 w-full items-center sm:items-start ">
-            <button
-              className={`button transition-colors w-full sm:w-auto duration-200 ease-in-out hover:shadow-md hover:bg-[#F0C808] hover:text-black font-[PPNeueMontreal] p-3 sm:p-5 rounded-full  text-2xl lg:text-[2.5vw] px-8 ${
-                isDark
-                  ? "text-[#1F1D1D] bg-[#FDFDFD]"
-                  : "text-[#FDFDFD] bg-[#1F1D1D]"
-              }`}
-            >
-              START A PROJECT
-            </button>
+            <Link href={"/start-a-project"}>
+              <button
+                className={`button transition-colors w-full sm:w-auto duration-200 ease-in-out hover:shadow-md hover:bg-[#F0C808] hover:text-black font-[PPNeueMontreal] p-3 sm:p-5 rounded-full  text-2xl lg:text-[2.5vw] px-8 ${
+                  isDark
+                    ? "text-[#1F1D1D] bg-[#FDFDFD]"
+                    : "text-[#FDFDFD] bg-[#1F1D1D]"
+                }`}
+              >
+                START A PROJECT
+              </button>
+            </Link>
             <div className="button flex space-x-3">
               <button
                 className={`w-14 ${
--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import Logo from "../../../assets/INCUBR.svg"
+import Logo from "@/assets/INCUBR.svg";
 
 export default function Headers() {
   return (
     <div className="flex w-full justify-center absolute top-0 left-0">
       <div className="w-[85%] flex justify-between mt-16 2xl:mt-20">
-        <Image src={Logo} />
-        <button className="hidden button rounded-full tracking-wider hover:shadow-md text-white bg-[#1F1D1D] hover:bg-[#F0C808] hover:text-black transition-colors ease-in-out duration-200 py-2 px-4 sm:flex items-center justify-center text-xl">
-          START A PROJECT
-        </button>
+        <Link href={"/"}>
+          <Image src={Logo} />
+        </Link>
+        <Link href={"/start-a-project"}>
+          <button className="hidden button rounded-full tracking-wider hover:shadow-md text-white bg-[#1F1D1D] hover:bg-[#F0C808] hover:text-black transition-colors ease-in-out duration-200 py-2 px-4 sm:flex items-center justify-center text-xl">
+            START A PROJECT
+          </button>
+        </Link>
       </div>
     </div>
   );
-};
+}
