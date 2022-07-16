ee96fe0c22eac1d715a1e88162d52c9d943fb7d8
Prabhat Ranjan <www.encodedprogrammer@gmail.com>
2022-07-17 00:01:28 +0530

    loading screen for mobile and tablet

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,20 +9,23 @@ import Lottie from "react-lottie";
 import Animated from "@/assets/lf20_fnddduvc.json";
 
 export default function Home() {
-  const { seconds, canShow } = React.useContext(Store);
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: Animated,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
+  const { seconds, canShow, height } = React.useContext(Store);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Animated,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <div className="w-full scroll-smooth">
       {canShow && (
-        <div className="fixed  loading-animation top-0 left-0 h-screen w-full bg-[#1D1D1D] z-[100]">
-          <div className="flex flex-1 justify-center h-full items-center">
+        <div
+          className="fixed  loading-animation top-0 left-0  w-full bg-[#1D1D1D] z-[100]"
+          style={{ height }}
+        >
+          <div className="flex flex-1 justify-center h-[80%] sm:h-[90%] lg:h-full items-center">
             <div className=" mt-16 lg:mt-0 px-6 text-white sm:px-16 lg:px-0">
               <div className=" font-[Arial] text-4xl sm:text-7xl lg:text-[4.3vw] flex flex-col">
                 <span className=" font-[Arial] text-4xl sm:text-7xl lg:text-[4.3vw]">
@@ -31,9 +34,9 @@ const defaultOptions = {
               </div>
             </div>
           </div>
-          <div className="flex w-full font-[PPNeueMontreal] lg:absolute bottom-20 justify-center">
+          <div className="flex w-full font-[PPNeueMontreal] absolute bottom-20 justify-center">
             <div className="flex w-[85%] items-end justify-center flex-col">
-              <div className=" cursor-pointer w-24 lg:w-36 relative h-24 lg:h-36 flex flex-col items-center justify-between">
+              <div className=" cursor-pointer w-32 lg:w-36 relative h-32 lg:h-36 flex flex-col items-center justify-between">
                 <div className=" w-full h-full animate-spin  bg-transparent  border-white hover:border-transparent transition-colors duration-300 ease-in-out hover:bg-[#F0C808] flex rounded-full border-b justify-center items-center"></div>
                 <span className="absolute text-white text-2xl h-full w-full flex justify-center items-center">
                   {seconds}%
